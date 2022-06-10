b32203e335cec359ab7ac4a53beaee0a5a48efb6
Douglas Humble <79142861+dollarkid1@users.noreply.github.com>
2022-06-10 17:30:55 +0100

    mh k

---
+++ src/main/java/com/hsquare/medicals/H2medicalApplication.java
@@ -0,0 +1,13 @@
+package com.hsquare.medicals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2medicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2medicalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hsquare/medicals/H2medicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsquare.medicals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2medicalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
